106408d0ed5e64a1b9d5de2a0e8b8ff6530523d2
sreeman <sreemanlingala333@gmail.com>
2025-03-21 19:33:12 +0530

    small edits

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 interface Testimonial {
   name: string;
@@ -9,55 +9,56 @@ interface Testimonial {
 
 const testimonials: Testimonial[] = [
   {
-    name: 'Priya',
-    title: 'Fashion designer',
-    testimonial: 'This product is amazing! It changed our Life style.',
-    avatar: 'https://randomuser.me/api/portraits/women/1.jpg',
+    name: "Priya",
+    title: "Fashion designer",
+    testimonial: "This product is amazing! It changed our Life style.",
+    avatar: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
-    name: 'Aakash',
-    title: 'Model',
-    testimonial: 'Beautiful scent,polishing smell.',
-    avatar: 'https://randomuser.me/api/portraits/men/1.jpg',
+    name: "Aakash",
+    title: "Model",
+    testimonial: "Beautiful scent,polishing smell.",
+    avatar: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
-    name: 'Rahul',
-    title: 'Model',
-    testimonial: 'Highly recommend! It improved My styling process dramatically.',
-    avatar: 'https://randomuser.me/api/portraits/men/2.jpg',
+    name: "Rahul",
+    title: "Model",
+    testimonial:
+      "Highly recommend! It improved My styling process dramatically.",
+    avatar: "https://randomuser.me/api/portraits/men/2.jpg",
   },
   {
-    name: 'Sanketh',
-    title: 'Businessmen',
-    testimonial: 'Great experience!I would recommend to others.',
-    avatar: 'https://randomuser.me/api/portraits/men/5.jpg',
+    name: "Sanketh",
+    title: "Businessmen",
+    testimonial: "Great experience!I would recommend to others.",
+    avatar: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
-    name: 'Ritika',
-    title: 'Beautician',
-    testimonial: 'Fantastic service, my team has been using it for months now!',
-    avatar: 'https://randomuser.me/api/portraits/women/6.jpg',
+    name: "Ritika",
+    title: "Beautician",
+    testimonial: "Fantastic service, my team has been using it for months now!",
+    avatar: "https://randomuser.me/api/portraits/women/6.jpg",
   },
   {
-    name: 'prasad',
-    title: 'businessmen',
-    testimonial: 'I can’t imagine going out without this. Truly a game changer.',
-    avatar: 'https://randomuser.me/api/portraits/men/5.jpg',
+    name: "prasad",
+    title: "businessmen",
+    testimonial:
+      "I can’t imagine going out without this. Truly a game changer.",
+    avatar: "https://randomuser.me/api/portraits/men/5.jpg",
   },
 ];
 
-export default function Testimonias () {
+export default function Testimonias() {
   const [currentTestimonial, setCurrentTestimonial] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTestimonial((prev) => (prev + 1) % testimonials.length);
-    }, 3000); 
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
-  
   const displayedTestimonials = [
     testimonials[(currentTestimonial + 0) % testimonials.length],
     testimonials[(currentTestimonial + 1) % testimonials.length],
@@ -68,9 +69,7 @@ export default function Testimonias () {
     <div className="max-w-7xl mx-auto p-8">
       <div className="relative overflow-hidden">
         <div className="absolute inset-0 bg-gray-100 opacity-50 z-0" />
-        <div
-          className="relative z-10 grid grid-cols-1 md:grid-cols-3 gap-8 transition-all transform duration-700"
-        >
+        <div className="relative z-10 grid grid-cols-1 md:grid-cols-3 gap-8 transition-all transform duration-700">
           {displayedTestimonials.map((testimonial, index) => (
             <div
               key={index}
@@ -92,6 +91,4 @@ export default function Testimonias () {
       </div>
     </div>
   );
-};
-
-
+}
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/olaperfumes",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
